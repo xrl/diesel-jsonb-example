8f2ed20794abc52e86b81aa7eb50a3aad0d5fdb0
Maria Dubyaga <kraftaa@gmail.com>
2020-02-13 12:41:10 -0800

    added nested jsonb&array

--- jsonb_table.sql
+++ jsonb_table.sql
@@ -7,4 +7,4 @@ CREATE TABLE jsonb_example (
 INSERT INTO jsonb_example
 (id,stringy,some_json)
 VALUES
-(1, 'hi mom', '{"hello": "there"}'), (2, 'hi dad', '{"hello": "to all the people"}');
+(1, 'hi mom', '{"hello": "there", "contract":[{"id":32, "name": "Agreement"}]}'), (2, 'hi dad', '{"hello": "to all the people",  "contract":null}');
--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,15 @@ fn main() {
 
 //    let res = jsonb_example_dsl::jsonb_example.filter(sql("some_json->'hello' ? 'to all the people'")).load::<JsonbExample>(&conn).unwrap();
 //    let res = jsonb_example_dsl::jsonb_example.select((jsonb_example_dsl::id, jsonb_example_dsl::some_json)).load::<(i32, serde_json::Value)>(&conn).unwrap();
-    type SmallerSelect = (i32, String);
-    let res = jsonb_example_dsl::jsonb_example.select((jsonb_example_dsl::id, sql("some_json->'hello'"))).load::<SmallerSelect>(&conn).unwrap();
+    type SmallerSelect = (i32, String, Option<i32>, Option<String> );
+    let res = jsonb_example_dsl::jsonb_example
+        .select((
+            jsonb_example_dsl::id,
+            sql("some_json->'hello'"),
+            sql("(some_json->'contract' -> 0 ->>'id')::int as id"),
+            sql("some_json->'contract' -> 0 ->>'name' as name"),
+        ))
+        .load::<SmallerSelect>(&conn).unwrap();
 
     println!("{:#?}", res);
 }
